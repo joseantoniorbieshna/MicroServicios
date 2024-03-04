5846b1b40a11d8f89342d51d4496f7ab6af0ae30
joseantoniorbieshna <jaramosb05@iesarroyoharnina.es>
2024-03-04 12:16:22 +0100

    Configuration working

---
+++ configserverMS/src/test/java/com/configserverms/ConfigserverMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserverms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- estudiantems/src/main/java/com/estudiantems/EstudianteController.java
+++ estudiantems/src/main/java/com/estudiantems/EstudianteController.java
@@ -1,6 +1,5 @@
 package com.estudiantems;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EstudianteController {
 	
 	@GetMapping("/uno")
-	public ResponseEntity<String> getEstudiante(){
-		return ResponseEntity.ok("Soy un estudiante");
+	public ResponseEntity<String> getEstudiante() {
+		return ResponseEntity.ok("soy un estudiante");
 	}
 }
